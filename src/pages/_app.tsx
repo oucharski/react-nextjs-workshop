aa7c3784bedc135fa29938ab31ad57a2933edef4
Felipe Oucharski <felipe.oucharski@andritz.com>
2023-03-21 18:24:35 +0100

    Add Layout to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Main, Header, Container, Footer } from "@/components";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+const appTitle = "Workshop Sessions";
+const footerContent = "Felipe Oucharski 2023";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Main>
+      <Header title={appTitle} />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+      <Footer>
+        <p>{footerContent}</p>
+      </Footer>
+    </Main>
+  );
 }
