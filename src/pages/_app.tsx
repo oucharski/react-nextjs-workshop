404def85cf9671bfbca8a94ad60a1f7750b631a2
Felipe Oucharski <felipe.oucharski@andritz.com>
2023-04-04 16:07:52 +0200

    Applied the theme to the Application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { Main, Header, Container, Footer } from "@/components";
 import "@/styles/globals.css";
+import { theme } from "@/styles/theme";
+import { ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 
 const appTitle = "Workshop Sessions";
@@ -7,14 +9,16 @@ const footerContent = "Felipe Oucharski 2023";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Main>
-      <Header title={appTitle} />
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-      <Footer>
-        <p>{footerContent}</p>
-      </Footer>
-    </Main>
+    <ThemeProvider theme={theme}>
+      <Main>
+        <Header title={appTitle} />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+        <Footer>
+          <p>{footerContent}</p>
+        </Footer>
+      </Main>
+    </ThemeProvider>
   );
 }
